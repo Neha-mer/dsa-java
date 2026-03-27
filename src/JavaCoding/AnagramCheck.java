0f627b6ff80d9e25a091a82f547e604b5c75b2a1
Neha Mer <merneha11@gmail.com>
2026-03-27 22:44:33 +0400

    Check if string are anagram

--- src/JavaCoding/AnagramCheck.java
+++ src/JavaCoding/AnagramCheck.java
@@ -3,10 +3,36 @@ package JavaCoding;
 public class AnagramCheck {
 
     public static void main(String[] args) {
-        String s="Silentgh";
-    String s2="isletn";
+        String s = "sillent";
+        String s2 = "tnesill";
 
+        s = s.toLowerCase();
+        s2 = s2.toLowerCase();
 
-     
+        if (s.length() != s2.length()) {
+            System.out.println("Not anagram");
+            return;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            count[s.charAt(i) - 'a']++;
+            System.out.println("kkk" + count[s.charAt(i) - 'a']);
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            count[s2.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                System.out.println("Not anagram");
+                return;
+            }
+        }
+
+        System.out.println("Anagram");
     }
 }
